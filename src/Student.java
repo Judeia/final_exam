c93c579d981c8cf0715a2e108e8dc4d4a1ece3a3
Jud <116132646+Judeia@users.noreply.github.com>
2022-12-28 20:13:11 +0800

    Create  Student.java

---
+++ src/Student.java
@@ -0,0 +1,16 @@
+public class Student
+{
+	// Data member
+	private String name;
+
+	/* Constructor */
+	public Student(String name) {
+		this.name = name;
+	}
+
+	// return the name of the student
+	public String getName() {
+		return name;
+	}
+
+}
